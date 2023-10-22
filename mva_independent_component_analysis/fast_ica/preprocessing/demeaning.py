9a08ea86254c34005692b36e61c8bedf30e7fea8
ylf <iivodka5@gmail.com>
2023-10-23 01:06:03 +0300

    gitignore

--- mva_independent_component_analysis/fast_ica/preprocessing/demeaning.py
+++ mva_independent_component_analysis/fast_ica/preprocessing/demeaning.py
@@ -7,4 +7,4 @@ def demeaning(X):
     :param X: data matrix of shape (n_features, n_samples)
     :return: centered data matrix
     """
-    return X - jnp.kron(jnp.ones(X.shape[1], ), jnp.mean(X, axis=1, keepdims=True))
+    return X - jnp.mean(X, axis=1, keepdims=True).reshape(X.shape[0], 1)
